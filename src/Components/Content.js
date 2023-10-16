a8fa2b92040a5a9bc850f14b4c984c0071b4ae62
Jim Grimes <jimgrimes86@gmail.com>
2023-10-16 16:52:14 -0400

    DONE: filter for managing reservations

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,10 +1,21 @@
-
+import { useState, useEffect } from "react";
 import ManageReservations from "./ManageReservations";
 
 function Content() {
+    const [reservations, setReservations] = useState([]);
+    const databaseURL = "http://localhost:3001"
+
+    useEffect(() => {
+        fetch(databaseURL+"/reservations")
+        .then(resp => resp.json())
+        .then(setReservations)
+    }, [])
+
+    console.log(reservations)
+
     return (
         <div>
-            <ManageReservations />
+            <ManageReservations reservations={reservations} />
         </div>
     )
 }
