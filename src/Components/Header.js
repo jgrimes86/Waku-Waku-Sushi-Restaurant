ae4e42d822530b50c991b3ee255b082950dfe5ed
Jim Grimes <jimgrimes86@gmail.com>
2023-10-17 12:44:13 -0400

    Added header image

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
+import WakuWaku from "./WakuWaku.png"
 
 function Header() {
     return (
-        <div>
-            <h1>Waku Waku</h1>
-            <h1>Sushi Restaurant</h1>
-        </div>
+        <img id="header-image" src={WakuWaku} alt="Waku Waku Sushi Restaurant" />
+        // <div>
+        //     <h1>Waku Waku</h1>
+        //     <h1>Sushi Restaurant</h1>
+        // </div>
     )
 }
 
