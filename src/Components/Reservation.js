e3b43a0e7b3f25fa62632cf1303a78f5b3ef55c7
LingZhouChen <lingzhouc@gmail.com>
2023-10-16 23:20:42 -0400

    updated db with test data

--- src/Components/Reservation.js
+++ src/Components/Reservation.js
@@ -1,32 +1,35 @@
+import { useState } from "react";
+
 function Reservation() {
+
+    const [friReserv, setFriReserv] = useState()
+
     return (
         <div className="reservation">
             <h2>Make A Reservation</h2>
             <form className="reservation-form">
-                <label for="date">Date  </label>
+                <label htmlFor="date">Date  </label>
                 <select name="date">
                     <option value="friday">Fri, Oct 20</option>
                     <option value="saturday">Sat, Oct 21</option>
                 </select>
 
-                <label for="date">Time </label>
+                <label htmlFor="date">Time </label>
                 <select name="time">
-                    <option value="1930">7:30 pm</option>
-                    <option value="2100">9:00 pm</option>
+                    <option value="7:30">7:30 pm</option>
+                    <option value="9:00">9:00 pm</option>
                 </select>
 
-                <label for="guests">Number of Guests  </label>
+                <label htmlFor="guests">Number of Guests  </label>
                 <select name="guests">
                     <option value="two-people">2 people</option>
                     <option value="three-people">3 people</option>
                     <option value="four-people">4 people</option>
-                    <option value="five-people">5 people</option>
-                    <option value="six-people">6 people</option>
                 </select>
 
-                <label for="name">Name  </label>
+                <label htmlFor="name">Name  </label>
                 <input type="text" required/>
-                <label for="number">Phone Number  </label>
+                <label htmlFor="number">Phone Number  </label>
                 <input type="text" required/>
 
                 <input type="submit" value="Make a Reservation"/>
