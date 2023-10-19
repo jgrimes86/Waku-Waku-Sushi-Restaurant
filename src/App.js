42a0940a779a6e1cf7fc6cbb009cd19f7ad9a674
LingZhouChen <lingzhouc@gmail.com>
2023-10-19 13:23:19 -0400

    add: footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-
 import Header from "./Components/Header";
 import NavBar from "./Components/NavBar";
+import Footer from "./Components/Footer";
 import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom"
 
@@ -90,6 +90,7 @@ function App() {
             <Header />
             <NavBar isLoggedIn={isLoggedIn} toggleLogout={toggleLogout}/>
             <Outlet context={context}/>
+            <Footer />
         </div>
     );
 }
