85d6bc007ea3bce0707a6cd47ee9db8585d1df13
LingZhouChen <lingzhouc@gmail.com>
2023-10-19 23:51:35 -0400

    add: finished styling of rez success

--- src/Components/ReservationComplete.js
+++ src/Components/ReservationComplete.js
@@ -7,16 +7,17 @@ function ReservationComplete() {
     const { activeRez } = useOutletContext();
 
     return (
-        <>
+        <div id="res-success">
             <h2>SUCCESS!</h2>
-            <div id="res-success">
-                <p>Your Reservation is set! We look forward to seeing you, {activeRez.name}!</p>
+            <div id="res-info">
+                <p id="res-name">Your Reservation is set! We look forward to seeing you, {activeRez.name}!</p>
+                
                 <p><BsCalendar3Event /> {activeRez.date}</p>
-                <p><BsPeopleFill /> {activeRez.guests} guests</p>
+                <p><BsPeopleFill /> {activeRez.guests} guest(s)</p>
                 <p><ImClock /> {activeRez.time}PM</p>
             </div>
             
-        </>
+        </div>
     )
 }
 
