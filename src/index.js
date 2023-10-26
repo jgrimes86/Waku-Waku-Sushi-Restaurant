dd9a357294fa4d8c67b31f96be77b26f4f6ef21d
Jim Grimes <jimgrimes86@gmail.com>
2023-10-26 12:42:21 -0400

    Uninstall express, delete app.use to start over

--- src/index.js
+++ src/index.js
@@ -4,15 +4,6 @@ import { RouterProvider } from "react-router-dom";
 import './stylesheets/index.css';
 import router from './data/router';
 
-const express = require('express');
-const app = express();
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-  });
-
 
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 root.render( <RouterProvider router={router} /> );
