a52db995c30e1872dce64bfa457bc9be1fdbabc4
Jim Grimes <jimgrimes86@gmail.com>
2023-10-26 14:15:28 -0400

    Deleted proxy value, trying CORS library

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import { RouterProvider } from "react-router-dom";
 import './stylesheets/index.css';
 import router from './data/router';
 
+const jsonServer = require('json-server')
+    
+const cors = require('cors');
+
+const server = jsonServer.create()       
+
+server.use(cors());
+
+
+
 
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 root.render( <RouterProvider router={router} /> );
