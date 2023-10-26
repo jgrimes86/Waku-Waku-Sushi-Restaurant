81dd60053fd16cb3c0047940ec2989dc02a205de
Jim Grimes <jimgrimes86@gmail.com>
2023-10-26 12:19:49 -0400

    installed express and added app.use to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import { RouterProvider } from "react-router-dom";
 import './stylesheets/index.css';
 import router from './data/router';
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
+
+
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 root.render( <RouterProvider router={router} /> );
 
