f29e52c6c0491d30f24c99c6a3eeacce5e779569
Jim Grimes <jimgrimes86@gmail.com>
2023-10-29 19:08:25 -0400

    Changed: 'server.use(cors({origin: '*'}))

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
 const middlewares = jsonServer.defaults();
 
-server.use(cors());
+server.use(cors({ origin: '*'}));
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
