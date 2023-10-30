986a32fd991cf7acb47a9a6bb9df19fece71e5ee
Jim Grimes <jimgrimes86@gmail.com>
2023-10-30 08:04:00 -0400

    change scripts for running development server and production server

--- server.js
+++ server.js
@@ -3,20 +3,14 @@ const cors = require("cors");
 const path = require("path");
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
-const middlewares = jsonServer.defaults({noCors: true});
+const middlewares = jsonServer.defaults();
 
 server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://waku-waku-sushi.netlify.app')
-  res.header('Access-Control-Allow-Headers', '*')
-  next()
-})
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`);
